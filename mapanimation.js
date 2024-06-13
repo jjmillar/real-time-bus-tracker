c225f8386d363d63a9fc5ec01f3979187bd19e1b
Julián Millar <julianjmillar@gmail.com>
2024-06-13 16:49:21 -0400

    chore: updated city bus

--- mapanimation.js
+++ mapanimation.js
@@ -21,7 +21,7 @@ const busStops = [
   let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-71.104081, 42.365554],
+    center: [-37.470759, -72.351294],
     zoom: 14,
   });
   
